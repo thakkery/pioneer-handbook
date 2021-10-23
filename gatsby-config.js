812c3374cad21d0cabd3fc7875c671a51bc58af0
Yatit Thakker <31781167+thakkery@users.noreply.github.com>
2021-10-23 08:37:44 -0700

    Change default info

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,14 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Nowispow`,
+    siteTitleAlt: `Nowispow - Knowledge is Power`,
+    siteHeadline: `Nowispow - The Web Development Guild`,
+    siteUrl: `https://nowispow.com`,
+    siteDescription: `Learn modern web development from beginner to advanced`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@nowispow`,
   },
   plugins: [
     {
@@ -31,16 +28,7 @@ module.exports = {
             slug: `/about`,
           },
         ],
-        externalLinks: [
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
-          },
-        ],
+        externalLinks: [],
       },
     },
     {
@@ -64,9 +52,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Nowispow - The Web Development Guild`,
+        short_name: `Nowispow`,
+        description: `Learn modern web development from beginner to advanced`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
@@ -131,7 +119,7 @@ module.exports = {
               }
             `,
             output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+            title: `Nowispow - @lekoarts/gatsby-theme-minimal-blog`,
           },
         ],
       },
